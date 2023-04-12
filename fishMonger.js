e7f4f67d4da53b241aabe5395656dbed938b00d5
Lance Buckley <erchancy@gmail.com>
2023-04-12 13:40:36 -0500

    Added data

---
+++ fishMonger.js
@@ -0,0 +1,82 @@
+/*
+Monger Inventory:
+
+This module must import boatInventory from the fishingBoat module 
+This module should also define and export its own function that generates its own inventory that 
+restaurants can use to purchase fish for their daily menus. The returned inventory should be 
+an array of objects.
+
+The exported function must be named mongerInventory.
+Each day, the fishmonger buys exactly 10 of each inexpensive (see below) fish caught by the 
+fishing boat. 
+If any of the fishing boat's fish quantity is less than 10, the fish monger does not buy it.
+The fishmonger does not buy any fish from the boat that is priced higher than $7.50 per fish.
+
+The fishmonger lets the chef of a restuarant specify what their maximum budget is per fish. 
+For example, a chef can tell the monger that she can only spend $5.00 per fish. The function 
+should then return only fish that cost $5.00 or less. This could be any amount and will be 
+information that will be provided to the monger inventory function.
+From your array of purchased fish, you must filter that down to the fish that meet the price 
+constraint of the restaurant chef.
+
+Once you have that final array of fish, return that array.
+
+Example of the filter array method
+const ages = [32, 33, 16, 40];
+const result = ages.filter(checkAdult);
+
+function checkAdult(age) {
+  return age >= 18;
+}
+*/
+
+//-------------------------------------------------------------------------------------------------
+
+//Import boatInventory
+    // const { boatInventory } = require("./fishingBoat.js")
+const { boatInventory } = require("./fishingBoat.js")
+
+/* Create function called mongerInventory(boatInventory, price)
+    the function iterates through and checks the object amount and price of boatInventory
+         Declare empty fishStock array
+            check if fish is lower than $7.50 per fish AND
+            check if amount of fish is at least 10 and if true, push quantity of 10 to fishStock
+          for fish of boatInventory
+              if fish.amount >= 10 && fish.price < 7.50 {
+                  fishStock.push({ id: fish.id, species: fish.species, weight: fish.weight, price: fish.price, amount: 10 })
+              }
+              
+              ////CHEFS REQUIREMENTS
+            const purchasedFish = fishStock.filter(fuction(budget)) {
+                return budget >= fishStock.price 
+         };
+         /////////////////////////////////// POSSIBLE ALTERNATIVE ///////////////////////////////
+         set empty array to purchasedFish
+         for fish of fishStock
+              if fish.price < price {
+                  purchasedFish.push({ id: fish.id, species: fish.species, weight: fish.weight, price: fish.price, amount: fish.amount * .5 })
+              }
+         //////////////////////////////////////////////////////////////////////////////////////////     
+         return purchasedFish
+          
+
+
+Export mongerInventory function
+         module.exports = { mongerInventory}
+*/
+
+const mongerInventory = (price) => {
+    fishStock = []
+    purchasedFish = []
+    for (fish of boatInventory())
+              if (fish.amount >= 10 && fish.price < 7.50) {
+                  fishStock.push({ id: fish.id, species: fish.species, weight: fish.weight, price: fish.price, amount: 10 })
+              }
+    for (fish of fishStock)
+        if (fish.price <= price) {
+                  purchasedFish.push({ id: fish.id, species: fish.species, weight: fish.weight, price: fish.price, amount: fish.amount * .5 })
+              }
+    return purchasedFish
+}
+
+module.exports = { mongerInventory}
---
+++ restaurant.js
@@ -0,0 +1,64 @@
+/* This module must import and invoke the function that is exported by the fish monger module. 
+This module should also define a function that build an HTML version of their daily menu.
+
+The function should return the generated HTML string. The function must be named fishMenu.
+
+Each day, the chef visits the monger and specifies the maximum price she is able to spend per fish.
+This price must be passed as an argument to the fishMenu function so that it can be passed along to 
+the fishmonger function when the chef buys the fish.
+
+If the monger has any fish at that price point or less, the chef buys 50% of the existing inventory. 
+This constraint will require you to update your fish monger logic!
+
+The chef then creates 3 different meals per fish -
+a soup, a grilled dinner, and sandwich. For example, if tuna was bought, the following meals will be 
+on the menu.
+
+Tuna Soup
+Tuna Sandwich
+Grilled Tuna
+*/
+
+/*
+Your HTML structure must match the example below. Use the same elements and the same class names.
+`<h1>Menu</h1>
+<article class="menu">
+    <h2>${ingredients.species}</h2>
+    <section class="menu__item">${ingredients.species} Soup</section>
+    <section class="menu__item">${ingredients.species} Sandwich</section>
+    <section class="menu__item">Grilled ${ingredients.species}</section>
+</article>`
+*/
+//-----------------------------------------------------------------------------------------------
+// Import mongerInventory
+    // const { mongerInventory } = require("./fishMonger")
+const { mongerInventory } = require("./fishMonger") 
+
+// Create fishMenu function with (budget) as param
+const fishMenu = (budget) => {
+    ingredients = mongerInventory(budget)
+    output = `<h1>Menu</h1>
+    <article class="menu">`
+    for (const menuItem of ingredients) {
+        output += 
+            `\n<h2>${menuItem.species}</h2>
+            <section class="menu__item">${menuItem.species} Soup</section>
+            <section class="menu__item">${menuItem.species} Sandwich</section>
+            <section class="menu__item">Grilled ${menuItem.species}</section>
+        </article>`
+        return output
+    }
+}
+    // Invoke the mongerInventory function passing a price as an argument
+        // declare ingredients = mongerInventory(budget)
+        
+        // create three dinners per fish bought using the ingredients array, returned in HTML
+            // a soup
+            // a grilled dinner
+            // a sandwhich
+
+
+// Export fishMenu
+    // module.exports = { fishMenu }
+
+module.exports = { fishMenu }
